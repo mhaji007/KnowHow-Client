fdc47a56d2af61b5faebe18f3d8a74153c3aa67b
Mehdi Hajikhani <mhaji007@fiu.edu>
2022-11-02 14:32:35 -0800

    Add bootstrap and ant design

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
 const Index = () => {
-    return (
-        <>
-            <h1>
-                Knowhow
-            </h1>
-        </>
-    )
-}
+  return (
+    <>
+      <h1 className="bg-primary py-5 d-flex justify-content-center dsiplay-1 text-light fw-bold shadow">Elevate</h1>
+    </>
+  );
+};
 
 export default Index;
