25b79d957736c56f3046778c051228be890946a9
Mehdi Hajikhani <mhaji007@fiu.edu>
2022-11-03 12:36:21 -0800

    Set up antd menu icons

--- components/TopNav.js
+++ components/TopNav.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { Menu } from "antd";
+import Link from "next/link";
+import {
+  AppstoreOutlined,
+  LoginOutlined,
+  UserAddOutlined,
+} from "@ant-design/icons";
+
+const { Item } = Menu;
 
 function TopNav() {
-  return <h1>TopNav</h1>;
+  return (
+    <Menu mode="horizontal">
+    <Item icon={<AppstoreOutlined />}>
+      <Link href="/">
+        App
+      </Link>
+    </Item>
+    <Item icon={<LoginOutlined />}>
+      <Link href="/login">
+        Login
+      </Link>
+    </Item>
+    <Item icon={<UserAddOutlined />}>
+      <Link href="/register">
+        Register
+      </Link>
+    </Item>
+  </Menu>
+);
 }
 
 export default TopNav;
