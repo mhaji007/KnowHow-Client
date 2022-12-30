937acd8c4ff10e1cd9a6913a24632508a2a2d1ef
Mehdi Hajikhani <mhaji007@fiu.edu>
2022-12-30 00:03:42 -0800

    Create becoming an instructor page

--- pages/user/become-instructor.js
+++ pages/user/become-instructor.js
@@ -1,11 +1,83 @@
+import { useContext, useState } from "react";
+import { Context } from "../../context";
+import { Button } from "antd";
 import axios from "axios";
+import {
+  SettingOutlined,
+  UserSwitchOutlined,
+  LoadingOutlined,
+} from "@ant-design/icons";
+import { toast } from "react-toastify";
+import UserRoute from "../../components/routes/UserRoute";
 
+// Displays a button that when clicked sends
+// a request to backend and backend in turn
+// sends a request to Stripe to create a new account for the user.
+// In response to this backend will receive a link 
+// which will be sent to the frontend. User will open a new
+// window upon clicking this link to complete the onboarding process
 const BecomeInstructor = () => {
+  // state
+  const [loading, setLoading] = useState(false);
+  const {
+    state: { user },
+  } = useContext(Context);
+
+  const becomeInstructor = () => {
+    // console.log("become instructor");
+    setLoading(true);
+    axios
+      .post("/api/make-instructor")
+      .then((res) => {
+        console.log(res);
+        // Open new window using new link retrieved from the backend
+        window.location.href = res.data;
+      })
+      .catch((err) => {
+        console.log(err.response.status);
+        toast("Stripe onboarding failed. Please try again.");
+        setLoading(false);
+      });
+  };
+
   return (
     <>
-      <h1 className="jumbotron h-100 p-5  text-center bg-primary square">
-        Become An Instructor
-      </h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 offset-md-3 text-center">
+            <div className="pt-4">
+              <UserSwitchOutlined className="display-1 pb-3" />
+              <br />
+              <h2>Setup payout to publish courses on KnowHow</h2>
+              <p className="lead text-danger">
+                KnowHow partners with Stripe to transfer earnings to your bank
+                account
+              </p>
+
+              <Button
+                className="mb-3"
+                type="primary"
+                block
+                shape="round"
+                icon={loading ? <LoadingOutlined /> : <SettingOutlined />}
+                size="large"
+                onClick={becomeInstructor}
+                disabled={
+                  (user && user.role && user.role.includes("Instructor")) ||
+                  loading
+                }
+              >
+                {loading ? "Processing..." : "Payout Setup"}
+              </Button>
+
+              <p className="lead">
+                You will be redirected to Stripe to complete onboarding process.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
