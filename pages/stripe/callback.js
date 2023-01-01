2b108aee658ac80e737637725a9e41f2b0ca2128
Mehdi Hajikhani <mhaji007@fiu.edu>
2022-12-31 22:04:45 -0800

    Create Stripe callback page

---
+++ pages/stripe/callback.js
@@ -0,0 +1,34 @@
+import { useContext, useEffect } from "react";
+import { Context } from "../../context";
+import { SyncOutlined } from "@ant-design/icons";
+import axios from "axios";
+// When user lands on this page,
+// request is made to the backend and in turn backend makes request to Stripe
+// to get the updated user information. This updated user information from Stripe
+// must have paymentEnabled set to true. At this point we can allow the user to
+// cerate courses since they have completed their Stripe setup
+const StripeCallback = () => {
+  const {
+    state: { user },
+  } = useContext(Context);
+
+  useEffect(() => {
+    if (user) {
+      // Fetch updated user information from Stripe
+      axios
+        .post(`${process.env.NEXT_PUBLIC_API}/get-account-status`)
+        .then((res) => {
+          window.location.href = "/instructor";
+        });
+    }
+  }, [user]);
+
+  return (
+    <SyncOutlined
+      spin
+      className="d-flex justify-content-center display-1 text-danger p-5"
+    />
+  );
+};
+
+export default StripeCallback;
